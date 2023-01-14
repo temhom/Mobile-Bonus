703a0cb77c1e679cc0be29bd37db7c208df09103
Artem Khomutskikh <artem7700@mail.ru>
2023-01-14 14:36:59 +0300

    variableUpdate

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-      int a = 500; // Начальная сумма на счету.
-      int b = 1000; // Сумма пополнения.
-      int c = b / 100; // Количество бонусов.
-      int d = a + b + c; // Итоговая сумма с бонусами.
-      int f = a + b; // Итоговая сумма без бонусов.
+        int accountSum = 500; // Начальная сумма на счету.
+        int plusSum = 1000; // Сумма пополнения.
+        int bonus = plusSum / 100; // Количество бонусов.
+        int summaryWithBonus = accountSum + plusSum + bonus; // Итоговая сумма с бонусами.
+        int summaryWithoutBonus = accountSum + plusSum; // Итоговая сумма без бонусов.
 
 
-        if(b >= 1000){
-            System.out.println("Поздравляем! Вы получили бонусы на свой счёт: " + (c) + " рублей.");
-            System.out.println("Общий баланс составляет: " + (d) + " рублей.");
-        }
-
-        else {
+        if (plusSum >= 1000) {
+            System.out.println("Поздравляем! Вы получили бонусы на свой счёт: " + (bonus) + " рублей.");
+            System.out.println("Общий баланс составляет: " + (summaryWithBonus) + " рублей.");
+        } else {
             System.out.println("Ваш бонусный счет: 0 рублей");
-            System.out.println("Общий баланс составляет: " + (f) + " рублей.");
+            System.out.println("Общий баланс составляет: " + (summaryWithoutBonus) + " рублей.");
         }
     }
 }
